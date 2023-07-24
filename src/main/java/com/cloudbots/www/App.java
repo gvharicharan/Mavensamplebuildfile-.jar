a935b83021e296560ecb04396a2d31fe7a728ec5
Haricharan <gvharicharan@gmail.com>
2023-07-24 04:43:10 +0000

    Modified changes to .jar build file

--- src/main/java/com/cloudbots/www/App.java
+++ src/main/java/com/cloudbots/www/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from cloudbots jar project Thankyou have a nice day" );
+        System.out.println( "Hello World! from cloudbots jar project...v2.0 Thankyou have a nice day" );
     }
 }
